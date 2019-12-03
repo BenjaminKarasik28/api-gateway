e3a44456359de5fac76404ed1f596e7188987cec
BenjaminKarasik28 <karasik.benjamin28@gmail.com>
2019-12-03 10:32:55 -0500

    jenkins push

--- src/test/java/com/example/apigateway/service/UserServiceTest.java
+++ src/test/java/com/example/apigateway/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.apigateway.service;
 import com.example.apigateway.Bean.UserBean;
 import com.example.apigateway.Repository.UserRepository;
 import com.example.apigateway.Service.UserService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.Assert.assertEquals;
@@ -20,12 +22,10 @@ import static org.mockito.Mockito.when;
 public class UserServiceTest {
 
     private UserBean user;
+    private UserDetails userDetails;
 
     @InjectMocks
     UserService userService;
-
-    @Mock
-    UserBean userBean;
 
     @Mock
     PasswordEncoder bCryptPasswordEncoder;
@@ -33,8 +33,22 @@ public class UserServiceTest {
     @Mock
     UserRepository userRepository;
 
+    @Before
+    public void initialize() {
+        user = new UserBean(1L, "testuser", "testpass");
+    }
+
     @Test
     public void loadUserByUsername_UserDetails_SUCCESS() {
         when(userRepository.findByUsername(any())).thenReturn(user);
+        when(bCryptPasswordEncoder.encode(any())).thenReturn("1d4f");
+        userDetails = userService.loadUserByUsername(user.getUsername());
+        assertEquals(user.getUsername(), userDetails.getUsername());
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    public void loadUserByUsernameNull_Error_FAIL() {
+        when(userRepository.findByUsername(any())).thenReturn(null);
+        userDetails = userService.loadUserByUsername(user.getUsername());
     }
 }
